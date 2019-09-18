a7c09a5bf57036aec8e43e4649d77509aa92c03c
tomjmcnamee <tom@tomjmcnamee.com>
2019-09-17 20:49:15 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,26 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
+function greet(hHMM) {
+  const hour = parseInt(hHMM);
+  switch (true) {
+    case (hour < 12 ):
+      return "Good Morning";
+      break;
+    case (hour >= 12 && hour <= 17):
+      return "Good Afternoon";
+      break;
+    case (hour >17 ):
+      return "Good Evening";
+      break;
+  }
+
+}
 /* Write your implementation of greet() */
+
+function displayMessage(string) {
+  const block = document.querySelector("#greeting")
+  block.innerText = string
+}
 /* Write your implementation of displayMessage() */
